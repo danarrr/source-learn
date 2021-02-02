90caf416341c96a4824b37f43d72c3b760116895
danarrr <867904420@qq.com>
2021-02-02 17:53:16 +0800

    redux

--- react/redux/src/store/index.js
+++ react/redux/src/store/index.js
@@ -1,5 +1,4 @@
 // import {createStore, applyMiddleware, combineReducers} from "redux";
-// import {createStore, applyMiddleware} from "../kredux/";
 import {createStore, applyMiddleware} from "../dredux/";
 // import thunk from "redux-thunk";
 // import logger from "redux-logger";
